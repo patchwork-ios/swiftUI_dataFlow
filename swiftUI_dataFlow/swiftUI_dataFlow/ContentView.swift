f42d2eec479b3175bb1d5a53a88e8b5cba460f46
patch-benjamin <benjaminarthurpatch@gmail.com>
2019-10-19 10:12:33 -0600

    Created XCode Project

---
+++ swiftUI_dataFlow/swiftUI_dataFlow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUI_dataFlow
+//
+//  Created by Benjamin Patch on 10/19/19.
+//  Copyright © 2019 Patchwork. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
